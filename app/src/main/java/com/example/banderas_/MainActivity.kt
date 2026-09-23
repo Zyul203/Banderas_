ccb732547c2fbb56d54bcbe33d9729808799cbe0
Luis <lagui@gmail.com>
2026-09-23 13:02:30 -0600

    Practica-09-Japon: Shape

--- app/src/main/java/com/example/banderas_/MainActivity.kt
+++ app/src/main/java/com/example/banderas_/MainActivity.kt
@@ -1,17 +1,21 @@
 package com.example.banderas_
-
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.Alignment //ALIGN
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.banderas_.ui.theme.Banderas_Theme
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.draw.clip //CLIP
+
 
 class MainActivity: ComponentActivity()
 {
@@ -20,21 +24,39 @@ class MainActivity: ComponentActivity()
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Bandera(modifier = Modifier.padding(innerPadding),)
-                }
+            Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    BanderaJapon(modifier = Modifier.padding(innerPadding))
             }
         }
     }
-
-fun Bandera(modifier: Modifier = Modifier)
-{
 }
 
-
+@Composable
+fun BanderaJapon(modifier: Modifier = Modifier)
+{
+    Box (modifier = modifier.fillMaxSize())
+    {
+        Box( //Box del fondo
+            modifier = Modifier
+                .fillMaxSize() //Para que ocupe tdo el espacio del BOX principal
+                .background(Color.White)
+        )
+        {
+            Box( //Box del circulo
+                modifier = Modifier
+                    .align(Alignment.Center)
+                    .size(200.dp)
+                    .clip(CircleShape)
+                    .background(Color.Red)
+            )
+        }
+    }
+}
 
 @Preview(showBackground = true)
 @Composable
-fun BanderaPreview() {
-    Bandera()
+fun BanderaJaponPreview() {
+    Surface {
+        BanderaJapon()
+    }
 }
