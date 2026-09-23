106e9f33af6a98247712f0bc3a916fb2a3e23f33
Luis <lagui@gmail.com>
2026-09-23 12:41:52 -0600

    Practica-06-Colombia: Row + Weight

--- app/src/main/java/com/example/banderas_/MainActivity.kt
+++ app/src/main/java/com/example/banderas_/MainActivity.kt
@@ -1,17 +1,23 @@
 package com.example.banderas_
-
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.snapping.SnapPosition
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.banderas_.ui.theme.Banderas_Theme
+import androidx.compose.ui.unit.dp
 
 class MainActivity: ComponentActivity()
 {
@@ -20,21 +26,28 @@ class MainActivity: ComponentActivity()
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Bandera(modifier = Modifier.padding(innerPadding),)
-                }
+            Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                BanderaColombia(modifier = Modifier.padding(innerPadding))
             }
         }
     }
-
-fun Bandera(modifier: Modifier = Modifier)
-{
 }
 
 
+@Composable
+fun BanderaColombia(modifier: Modifier = Modifier)
+{
+    Row(modifier = modifier.fillMaxSize()) {
+        Box(Modifier.weight(2f).fillMaxHeight().background(colorResource(id = R.color.amarillo_colombia)))
+        Box(Modifier.weight(1f).fillMaxHeight().background(colorResource(id = R.color.azul_colombia)))
+        Box(Modifier.weight(1f).fillMaxHeight().background(colorResource(id = R.color.rojo_colombia)))
+    }
+}
 
 @Preview(showBackground = true)
 @Composable
-fun BanderaPreview() {
-    Bandera()
+fun BanderaColombiaPreview() {
+    Surface {
+        BanderaColombia()
+    }
 }
