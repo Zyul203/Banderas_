92a615b85697eeccf288ddb9ca439ddfdd954b07
Luis <lagui@gmail.com>
2026-09-23 13:10:23 -0600

    Practica-11-Chile: Icon + Row + Column

--- app/src/main/java/com/example/banderas_/MainActivity.kt
+++ app/src/main/java/com/example/banderas_/MainActivity.kt
@@ -4,14 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.banderas_.ui.theme.Banderas_Theme
+
+import androidx.compose.material3.Icon
+import androidx.compose.ui.unit.dp
+
+import androidx.compose.material.icons.Icons //Libreria de Iconos
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.ui.draw.rotate // Rotar
 
 class MainActivity: ComponentActivity()
 {
@@ -20,21 +28,63 @@ class MainActivity: ComponentActivity()
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Bandera(modifier = Modifier.padding(innerPadding),)
-                }
+            Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    BanderaChile(modifier = Modifier.padding(innerPadding))
             }
+
         }
     }
-
-fun Bandera(modifier: Modifier = Modifier)
-{
 }
 
-
+@Composable
+fun BanderaChile(modifier: Modifier = Modifier)
+{
+    Row (modifier = modifier.fillMaxSize())
+    {
+        Column(modifier = Modifier
+            .weight(1f)
+            .fillMaxWidth()
+        ) {
+            Box(
+                modifier = Modifier
+                    .weight(.66f)
+                    .fillMaxHeight()
+                    .background(Color.White)
+            )
+            Box(
+                modifier = Modifier
+                    .weight(.33f)
+                    .fillMaxWidth()
+                    .background(Color.Blue),
+                contentAlignment = Alignment.Center
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Star, //Se usa imageVector en los iconos
+                    contentDescription = "Estrella de Chile",
+                    tint = Color.White, // tint es usado para cambiar color del icono
+                    modifier = Modifier.size(150.dp).rotate(200f)
+                )
+                //Icons.Default. Para ver los iconos disponibles
+            }
+        }
+        Box(
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxHeight()
+                .background(Color.Red)
+        )
+    }
+}
 
 @Preview(showBackground = true)
 @Composable
-fun BanderaPreview() {
-    Bandera()
+fun BanderaChilePreview() {
+    Surface {
+        BanderaChile()
+    }
 }
+
+/* NOTA: Agregar estas dependencias en la seccion de Gradle Scripts (build.gradle.kts) y darle "Sync Now"
+    implementation("androidx.compose.material:material-icons-core")
+    implementation("androidx.compose.material:material-icons-extended") //Mas iconos
+ */
