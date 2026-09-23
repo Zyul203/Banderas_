3742bbb0709b1bb6803494de7d126c46b73fccb3
Luis <lagui@gmail.com>
2026-09-23 11:58:18 -0600

    Practica-04-Alemania: Column + Row save1

--- app/src/main/java/com/example/banderas_/MainActivity.kt
+++ app/src/main/java/com/example/banderas_/MainActivity.kt
@@ -13,8 +13,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.res.colorResource
-
 
 class MainActivity: ComponentActivity()
 {
